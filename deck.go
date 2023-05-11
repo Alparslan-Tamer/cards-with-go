853fc4b17050a15a74fbe9182484a4e98e79c7b5
Alparslan Tamer <alparslantamermain@gmail.com>
2023-05-12 00:09:23 +0300

    shuffle feature for deck added.

--- deck.go
+++ deck.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"os"
 	"strings"
+	"time"
 )
 
 // Create a new type of 'deck'
@@ -48,6 +50,21 @@ func newDeckFromFile(filename string) deck {
 	if err != nil {
 		// Log the error and entirely quit the program
 		fmt.Println("Error: ", err)
-		
+		os.Exit(1)
+	}
+
+	s := strings.Split(string(bs), ",")
+
+	return deck(s)
+}
+
+func (d deck) shuffle() {
+	source := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(source)
+
+	for i := range d {
+		newPosition := r.Intn(len(d) - 1)
+
+		d[i], d[newPosition] = d[newPosition], d[i]
 	}
 }
